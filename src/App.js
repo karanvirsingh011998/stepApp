e599dbb76e08b04a65530298b419d102511e8ff3
unknown <Karanvir011998@gmail.com>
2024-01-09 23:26:57 +0530

    step app final version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  console.log(step);
+  const message = ["Learn React", "Apply for jobs", "Invest your new income"];
+  const previousHandler = () => {
+    setStep((step)=>step - 1);
+  };
+  const nextHandler = () => {
+    setStep((step)=>step + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button className="close" onClick={()=>setIsOpen(!isOpen)}>{!isOpen? 'Open':'Close'}</button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step} :{message[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              disabled={step == 1}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => previousHandler()}
+            >
+              Previous
+            </button>
+            <button
+              disabled={step == 3}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={() => nextHandler()}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
